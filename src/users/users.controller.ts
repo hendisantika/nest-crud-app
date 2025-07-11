4d8779be2a103e905d3be802b62094b5a531ce99
hendisantika <hendisantika@yahoo.co.id>
2025-07-12 06:32:51 +0700

    feat: Add findAll in user controller🫸🌀✏️📗🐧🐳

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  //get all users
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
 }
